516a5bfbcfaba089cd1a75a51694e62400a87b7c
Abdurrofi'i Assajid Arbya <abdurrofii.aas@gmail.com>
2024-10-18 12:41:54 +0700

    auth-UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Example from "@/components/Example";
+
+import LoginForm from '@/components/Login'
 
 export default function Home() {
   return (
     <main>
-      <Example/>
+      <LoginForm/>
     </main>
   );
 }
