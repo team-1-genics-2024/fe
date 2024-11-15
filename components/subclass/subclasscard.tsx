7fa8319535cf9f0ed9a73b9d2a522aca39c6cf77
Nichonicholass <nicholassamuel34@gmail.com>
2024-11-15 23:13:46 +0700

    add: fix subclass page

--- components/subclass/subclasscard.tsx
+++ components/subclass/subclasscard.tsx
@@ -14,20 +14,11 @@ export default function SubClassCard({
   textbook,
 }: SubClassCardProps) {
   return (
-    <div>
-      {/* <section className="max-md:justify-start max-md:items-center max-md:flex max-md:flex-col md:px-[90px]"> */}
-      <section className="px-9 md:px-[90px]">
-        <h1 className="text-[24px] md:text-[45px]">{judul}</h1>
-        <div className="mt-2 md:mt-4">
-          {/* Wrapper untuk membuat video responsif */}
-          <div className="relative  md:pt-[56.25%] max-w-full max-md:w-[309px] max-md:h-[174px] md:w-[900px] md:h-[485px]">
-            {/* <ReactPlayer
-              url="https://www.youtube.com/watch?v=9Yfj7dmcmXA"
-              controls
-              width="100%"
-              height="100%"
-              className="absolute top-0 left-0 w-full h-full"
-            /> */}
+    <div className="w-fit">
+      <section className="px-9 md:px-[50px] lg:px-[90px] w-fit">
+        <h1 className="text-[24px] md:text-[45px]" id="course-video">{judul}</h1>
+        <div className="mt-2 md:mt-4 w-fit">
+          <div className="relative  md:pt-[56.25%] max-w-full w-[309px] h-[174px] md:w-[500px] md:h-[285px] lg:w-[833px] lg:h-[485px]">
             <ReactPlayer
               url={video}
               controls
@@ -38,8 +29,8 @@ export default function SubClassCard({
           </div>
         </div>
       </section>
-      <section className="px-9 mt-5 md:px-[90px] md:mt-24">
-        <div>
+      <section className="px-9 mt-5 md:px-[50px] lg:px-[90px] md:mt-24">
+        <div id="text-book">
           <p className="text-[22px] md:text-[32px]">Text Book Materi</p>
         </div>
         <div className="text-[14px] mt-2 md:text-[16px]">{textbook}</div>
