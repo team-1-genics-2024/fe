a3e613c929b7d608bc496a8295c0ef6a85783177
Fth87 <fatihfwz87@gmail.com>
2024-11-29 07:17:39 +0700

    minor change

--- components/dasboard/MyCourse.tsx
+++ components/dasboard/MyCourse.tsx
@@ -4,7 +4,7 @@ const MyCourse = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" h-[100vh] md:h-[80vh] md:max-w-[800px] bg-[#F7FCFF] lg:px-12 xl:px-14 pt-6 pb-14 ">
       <h1 className="text-4xl font-medium mb-8 px-7">My Course</h1>
-      <ScrollArea className="px-7 h-[90%]">
+      <ScrollArea className="px-7 h-[90%] overflow-y-scroll">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 ">{children}</div>
       </ScrollArea>
     </div>
